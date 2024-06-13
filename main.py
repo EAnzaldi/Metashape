ed2458afb45b5840df97cfa9934186fb48773cc8
EAnzaldi <elisabettaanzaldi1@gmail.com>
2024-06-13 10:03:46 +0200

    Added point cloud filtering

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 import Metashape as meta
 
-#Matching accuracy (downscaling by factor of x)
+#alignment accuracy (each side x*original)
 Highest = 0
 High = 1
 Medium = 2
 Low = 4
 Lowest = 8
-#Depths maps accuracy
+
+#depths maps accuracy
 Ultra_dep = 1
 High_dep = 2
 Medium_dep = 4
 Low_dep = 8
 Lowest_dep = 16
 
-doc = meta.app.document #currently opened document in actual Metashape window
-""" try: doc.open("D:\ellyb\POLITO\03\Droni\Esercitazioni\Py\PP1.psz")
-except RuntimeError:
-    meta.Application.messageBox("Can't open project")"""
+#maximum confidence filtered
+maxc=3;
 
-meta.Application.gpu_mask=1; #1 GPU activated (binary value) 
+doc = meta.app.document #currently opened document
 chunk=doc.chunk
-#for chunk in doc.chunks:
-    #Align photos   
+
 chunk.matchPhotos(downscale = Medium, #accuracy
-generic_preselection = True,
-filter_stationary_points = True,
+generic_preselection = True, #default
+filter_stationary_points = True, #default
 keypoint_limit = 40000, #default
 tiepoint_limit = 4000) #default
-#Align cameras
 chunk.alignCameras()
-#doc.save()
-chunk.buildDepthMaps(downscale=Medium_dep, filter_mode=meta.MildFiltering, reuse_depth=True)
-chunk.buildPointCloud(point_colors=True, point_confidence=True)
 
+chunk.buildDepthMaps(
+    downscale=Medium_dep, #accuracy
+    filter_mode=meta.MildFiltering, #default
+    reuse_depth=True) #enable reuse depth maps option
+chunk.buildPointCloud(
+    point_colors=True, #default
+    point_confidence=True) #enable point confidence calculation
 
+pc = chunk.point_cloud #point cloud just generated
+pc.setConfidenceFilter(0, maxc) #filter only low-confidence points
+pc.removePoints(list(range(128))) #removes activated points
+pc.resetFilters() #reset to activate remaining points
